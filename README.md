7287919dac76d97ea5a8418e80ddebc88cc8c925
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-04-08 11:10:12 -0400

    initial import

---
+++ README.md
@@ -0,0 +1,16 @@
+> **NOT YET FUNCTIONAL**
+
+# ti-2048
+
+A Titanium Alloy port of Gabriele Cirulli's [2048](http://gabrielecirulli.github.io/2048/) game.
+
+## Supports
+
+* iOS
+
+## Why?
+
+2 reasons:
+
+1. Why not?
+2. I was really disappointed with ineraction and performance in the web version on my mobile devices. By levergaing Titanium's cross-platform API I can give the same smooth experience I had on desktop to my mobile devices. Additionally, the transition from web-based game to Titanium will be much easier by using Alloy.
---
+++ app/controllers/index.js
@@ -0,0 +1,26 @@
+var tiles = [],
+	offsets = [0, 68, 135, 203];
+
+// for (var y = 0; y < 4; y++) {
+// 	for (var x = 0; x < 4; x++) {
+// 		var tile = Ti.UI.createView({
+// 			height: 57.5,
+// 			width: 57.5,
+// 			top: offsets[y],
+// 			left: offsets[x],
+// 			borderRadius: 3,
+// 			background: '#eee4da',
+// 			textAlign: 'center',
+// 			font: {
+// 				fontWeight: 'bold',
+// 				fontSize: 55
+// 			},
+// 			zIndex: 10,
+// 		});
+// 		$.grid.add(tile);
+// 		!tiles[x] && (tiles[x] = []);
+// 		tiles[x][y] = tile;
+// 	}
+// }
+
+$.index.open();
